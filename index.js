a5be0d6f23d3a1656bcfbb9d5bff3a91ef8829b1
ChamodJ <121800679+ChamodJ@users.noreply.github.com>
2023-04-07 16:59:59 +0530

    Update JS

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ for (i = 0; i < answer.length; i++){
 
     if(content.style.display === "block") {
         content.style.display = "none"
+        event.target.style.color="hsl(240, 6%, 50%)"
     }
     else {
         content.style.display = "block"
+        event.target.style.color="hsl(238, 29%, 16%)"
     }
 
     })
